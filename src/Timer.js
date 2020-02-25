e0f9014e891688c927cf776109943c848283e8f0
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-02-24 19:14:49 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Timer extends Component {
   state = {
     time: 0,
-    color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+    color: '#' + Math.floor(Math.random() * 16777215).toString(16)
   };
 
   // add your code here
@@ -11,13 +11,17 @@ class Timer extends Component {
   render() {
     const { time, color } = this.state;
     return (
-      <section className="Timer" style={{ background: color }}>
+      <section className='Timer' style={{ background: color }}>
         <h1>{time}</h1>
         <button onClick={this.stopClock}>Stop</button>
-        <aside className="mountText">Mounted</aside>
+        <aside className='mountText'>Mounted</aside>
         <small onClick={this.handleClose}>X</small>
       </section>
     );
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000);
   }
 
   //clock functions
@@ -26,6 +30,10 @@ class Timer extends Component {
       time: prevState.time + 1
     }));
   };
+
+  componentWillUnmount() {
+    this.stopClock();
+  }
 
   stopClock = () => {
     clearInterval(this.interval);
